ccc8b0f75102a25d68a1df0baedb5199e4e6a3df
WeremyJalters <jeremyjwalters@gmail.com>
2014-07-27 18:00:48 -0400

    created Note model with association to user

---
+++ db/migrate/20140727215852_create_notes.rb
@@ -0,0 +1,10 @@
+class CreateNotes < ActiveRecord::Migration
+  def change
+    create_table :notes do |t|
+      t.string :body
+      t.belongs_to :user, index: true
+
+      t.timestamps
+    end
+  end
+end
